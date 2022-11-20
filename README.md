9e07eb6cef3c25a91bf937c8a7f9f77d95f89fe4
Danylo Altukhov <dan.97198@gmail.com>
2022-11-20 18:48:17 +0200

    done task

---
+++ README.md
@@ -0,0 +1,11 @@
+First Step:
+https://monosnap.com/file/fm0XoPZTlmBdh9kQJdehuw2uYM6hFX
+
+Second Step:
+https://monosnap.com/file/S56iOhs4101dOhchX3v9YdTGYWcMdK
+
+Third Step:
+https://monosnap.com/file/dgDIkCGljXXxWaHy6lPJyqcdlJFbcu
+
+Fourth Step:
+https://monosnap.com/file/roKyo1irL6KwQFmRy5uah8PnOQ9Glt
---
+++ index.js
@@ -0,0 +1,32 @@
+const {
+  listContacts,
+  getContactById,
+  removeContactById,
+  addContact,
+} = require("./contact");
+const argv = require("yargs").argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+    case "remove":
+      const removedContact = await removeContactById(id);
+      console.log(removedContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
